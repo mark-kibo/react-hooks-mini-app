6e4a808112fd10d5639ab6ac445e498723c88e45
mark-kibo <kibochamark@gmail.com>
2023-08-27 22:30:39 -0700

    Feat:added navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+    <NavLink to={}></NavLink>
+  )
+}
+
+export default NavBar
